376001f3feb70c83bbe1b0fb73610898c2310030
Your Name <you@example.com>
2017-09-02 00:37:45 -0400

    Display gyms around cleveland in map

---
+++ ExerciseGO/Scripts/Map.js
@@ -0,0 +1,40 @@
+﻿var map;
+var infowindow;
+
+function initMap() {
+    var pyrmont = { lat: 41.4993, lng: -81.6944 };
+
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: pyrmont,
+        zoom: 14
+    });
+
+    infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+        location: pyrmont,
+        radius: 5000,
+        type: ['gym']
+    }, callback);
+}
+
+function callback(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+            createMarker(results[i]);
+        }
+    }
+}
+
+function createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+    });
+
+    google.maps.event.addListener(marker, 'click', function () {
+        infowindow.setContent(place.name);
+        infowindow.open(map, this);
+    });
+}
