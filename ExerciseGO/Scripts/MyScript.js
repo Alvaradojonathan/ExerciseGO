cfa64502751f7b6bfddd8c376407acdc779cc1cd
Woodsmike <Woodsmike@users.noreply.github.com>
2017-09-05 14:14:00 -0400

    map

--- ExerciseGO/Scripts/MyScript.js
+++ ExerciseGO/Scripts/MyScript.js
@@ -1,18 +1,19 @@
 ﻿$(document).ready(function () {
-    
-    var progressPoints = (pointsEarned / 50000) * 100;
+
+
+    //var progressPoints = (pointsEarned / 50000) * 100;
    
-    function move() {
-        var elem = document.getElementById("myBar");
-        var width = progressPoints;
-        var id = setInterval(frame, 10);
-        function frame() {
-            if (width >= 100) {
-                clearInterval(id);
-            } else {
-                width = progressPoints;
-            }
-        }
-    }
+    //function move() {
+    //    var elem = document.getElementById("myBar");
+    //    var width = progressPoints;
+    //    var id = setInterval(frame, 10);
+    //    function frame() {
+    //        if (width >= 100) {
+    //            clearInterval(id);
+    //        } else {
+    //            width = progressPoints;
+    //        }
+    //    }
+    //}
     
 });
