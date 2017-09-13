26b388a86f8b3b617b385987f270b0f8672b7715
Your Name <you@example.com>
2017-09-12 23:59:51 -0400

    allow log delete

--- ExerciseGO/Scripts/Map.js
+++ ExerciseGO/Scripts/Map.js
@@ -1,43 +1,43 @@
-﻿    var map;
-    var infowindow;
+﻿var map;
+var infowindow;
 
-    function initMap() {
-        var pyrmont = { lat: 41.4993, lng: -81.6944 };
+function initMap() {
+    var pyrmont = { lat: 41.4993, lng: -81.6944 };
 
-        map = new google.maps.Map(document.getElementById('map'), {
-            center: pyrmont,
-            zoom: 14
-        });
+    map = new google.maps.Map(document.getElementById('map'), {
+        center: pyrmont,
+        zoom: 14
+    });
 
-        infowindow = new google.maps.InfoWindow();
-        var service = new google.maps.places.PlacesService(map);
-        service.nearbySearch({
-            location: pyrmont,
-            radius: 5000,
-            type: ['gym']
-        }, callback);
-    }
+    infowindow = new google.maps.InfoWindow();
+    var service = new google.maps.places.PlacesService(map);
+    service.nearbySearch({
+        location: pyrmont,
+        radius: 5000,
+        type: ['gym']
+    }, callback);
+}
 
-    function callback(results, status) {
-        if (status === google.maps.places.PlacesServiceStatus.OK) {
-            for (var i = 0; i < results.length; i++) {
-                createMarker(results[i]);
-            }
+function callback(results, status) {
+    if (status === google.maps.places.PlacesServiceStatus.OK) {
+        for (var i = 0; i < results.length; i++) {
+            createMarker(results[i]);
         }
     }
+}
 
-    function createMarker(place) {
-        var placeLoc = place.geometry.location;
-        var marker = new google.maps.Marker({
-            map: map,
-            position: place.geometry.location
-        });
+function createMarker(place) {
+    var placeLoc = place.geometry.location;
+    var marker = new google.maps.Marker({
+        map: map,
+        position: place.geometry.location
+    });
 
-        google.maps.event.addListener(marker, 'click', function () {
-            infowindow.setContent(place.name);
-            infowindow.open(map, this);
-        });
-    }
+    google.maps.event.addListener(marker, 'click', function () {
+        infowindow.setContent(place.name);
+        infowindow.open(map, this);
+    });
+}
 
 
 //display legs div
@@ -239,18 +239,6 @@ function closeWindowFive() {
     }
 }
 
-//Activity Log filtering
-$(document).ready(function () {
-    $('.results > li').hi();
-
-    $('div.tags').find('input:checkbox').live('click', function () {
-        $('.results > li').hide();
-        $('div.tags').find('input:checked').each(function () {
-            $('.results > li.' + $(this).attr('rel')).show();
-        });
-    });
-});
-
 //UPPERCASE ids of Arms, Legs, etc. is for the Activity log page....lowercase for home page
 function openArmsDiv() {
 
@@ -315,15 +303,3 @@ function openFilterDiv() {
         x.style.display = 'block';
     }
 }
-
-//Activity Log filtering
-$(document).ready(function () {
-    $('.results > li').hi();
-
-    $('div.tags').find('input:checkbox').live('click', function () {
-        $('.results > li').hide();
-        $('div.tags').find('input:checked').each(function () {
-            $('.results > li.' + $(this).attr('rel')).show();
-        });
-    });
-});
